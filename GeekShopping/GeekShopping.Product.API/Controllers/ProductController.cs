1f28d0d07e59b772357ed3dcb814dc8b8e712ab3
Thiago Jambeiro <thiago_jamb@hotmail.com>
2023-07-12 14:57:48 -0300

    Inicio implementação do layout

--- GeekShopping/GeekShopping.Product.API/Controllers/ProductController.cs
+++ GeekShopping/GeekShopping.Product.API/Controllers/ProductController.cs
@@ -32,7 +32,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(ProductVO vo)
+        public async Task<IActionResult> Create([FromBody] ProductVO vo)
         {
             if (vo == null) return BadRequest();
             var product = await _repository.Create(vo);
@@ -41,7 +41,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpPut]
-        public async Task<IActionResult> Update(ProductVO vo)
+        public async Task<IActionResult> Update([FromBody] ProductVO vo)
         {
             if (vo == null) return BadRequest();
             var product = await _repository.Update(vo);
